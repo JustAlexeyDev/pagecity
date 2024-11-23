3ab6b6c3963a4197358f9869535d785022c3a163
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-11-24 01:52:49 +0900

    Update Header.jsx

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,22 +1,24 @@
 import './Header.css';
 import logo from '../../Assets/Images/logo.svg';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 const Header = () => {
     const [activeButton, setActiveButton] = useState(null);
-
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleClick = (event) => {
         const buttonId = event.currentTarget.id;
         setActiveButton(buttonId);
     }
 
-    return(
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    }
+
+    return (
         <div className="Header--Container">
-            <button>
-                <img src={logo} alt="Logo"/>
-            </button>
             <span className='Header--Container__Nav--Block'>
+
                 <button 
                     id='1' 
                     onClick={handleClick}
@@ -48,10 +50,51 @@ const Header = () => {
                     className={activeButton === '6' ? 'accent--color' : ''}
                 >Услуги</button>
             </span>
-            <button>
+            <button className='Header--Container__Item'>
                 Личный Кабинет
             </button>
+            <div className='Header--Container__Burger' onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className={`Header--Container__Popup ${isMenuOpen ? 'open' : ''}`}>
+                <button 
+                    id='1' 
+                    onClick={handleClick}
+                    className={activeButton === '1' ? 'accent--color' : ''}
+                >Главная</button>
+                <button 
+                    id='2' 
+                    onClick={handleClick}
+                    className={activeButton === '2' ? 'accent--color' : ''}
+                >Проекты</button>
+                <button 
+                    id='3'  
+                    onClick={handleClick}
+                    className={activeButton === '3' ? 'accent--color' : ''}
+                >Жалобы</button>
+                <button 
+                    id='4'  
+                    onClick={handleClick}
+                    className={activeButton === '4' ? 'accent--color' : ''}
+                >Контакты</button>
+                <button 
+                    id='5'  
+                    onClick={handleClick}
+                    className={activeButton === '5' ? 'accent--color' : ''}
+                >Форум</button>
+                <button 
+                    id='6'  
+                    onClick={handleClick}
+                    className={activeButton === '6' ? 'accent--color' : ''}
+                >Услуги</button>
+                <button className='Header--Container__Item'>
+                    Личный Кабинет
+                </button>
+            </div>
         </div>
     );
 }
+
 export default Header;
