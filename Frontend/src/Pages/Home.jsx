e9c108d0528ce11d5c33ff395434497ae33dc2e4
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-11-24 01:53:00 +0900

    Update Home.jsx

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,9 +1,13 @@
 import Header from "../Components/Header/Header";
+import Slider from "../Components/Slider/Slider";
+import VideoBlock from "../Components/VideoBlock/VideoBlock";
 
 const Home = () => {
     return(
         <div>
             <Header />
+            <Slider />
+            <VideoBlock />
         </div>
     );
 }
