3a9edcb6aff76ac54c5c07e6328ea61090d475de
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-11-23 23:54:08 +0900

    Update Home.jsx

--- Frontend/src/Pages/Home.jsx
+++ Frontend/src/Pages/Home.jsx
@@ -1,11 +1,9 @@
 import Header from "../Components/Header/Header";
-import HeroSlider from "../Components/HeroSlider/HeroSlider";
 
 const Home = () => {
     return(
         <div>
             <Header />
-            <HeroSlider/>
         </div>
     );
 }
