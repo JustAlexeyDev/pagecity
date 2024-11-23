a4445136dab7664ee56fdb9deb01d3e4d3a7e7a1
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-11-24 06:00:34 +0900

    Create authRoutes.js

---
+++ Backend/routes/authRoutes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const { login } = require('../controllers/authController');
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * /api/login:
+ *   post:
+ *     summary: Authenticate user
+ *     tags:
+ *       - Authentication
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Successful login
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       401:
+ *         description: Invalid credentials
+ */
+router.post('/login', login);
+
+module.exports = router;
