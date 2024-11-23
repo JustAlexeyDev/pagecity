2278912027d42d5ae9c263655c5078d410413e56
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-11-24 04:19:07 +0900

    Update Service.jsx

--- Frontend/src/Components/Service/Service.jsx
+++ Frontend/src/Components/Service/Service.jsx
@@ -1,23 +1,30 @@
 import './Service.css';
 
+import Bussines from '../../Assets/Images/Services/Bussines.svg';
+import Plan from '../../Assets/Images/Services/Plan.svg';
+import Bricks from '../../Assets/Images/Services/Bricks.svg';
+
 const Service = () => {
     return(
-        <div className="Service--Container">
+        <div className="Service--Container Component">
             <div>
                 <h1>Наши услуги</h1>
             </div>
             <div className='Service--Container--Block'>
 
                 <div className='Service--Container--Block__Item'>
+                    <img src={Bricks} alt='icon'/>
                     <p>Реконструкции</p>
                 </div>
 
                 <div className='Service--Container--Block__Item'>
+                    <img src={Plan} alt='icon'/>
                     <p>Разработка проекта</p>
                 </div>
 
 
                 <div className='Service--Container--Block__Item'>
+                    <img src={Bussines} alt='icon'/>
                     <p>Сопровождение бизнес проектов</p>
                 </div>
 
