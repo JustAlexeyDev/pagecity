e36dc41de780f6d8396f2a7be82077e4da511465
Just Alex <just.alexdev@gmail.com>
2024-11-23 11:58:41 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,9 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
 import App from "./App";
 
+import "./Assets/Css/Global.css";
+import "./Assets/Css/Reset.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
