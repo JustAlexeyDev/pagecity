35220bb4337e358c0b842e30a3ced355f3a4a82a
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-11-24 02:32:14 +0900

    Update Slider.jsx

--- Frontend/src/Components/Slider/Slider.jsx
+++ Frontend/src/Components/Slider/Slider.jsx
@@ -1,13 +1,5 @@
 import './Slider.css';
-// import Swiper core and required modules
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import 'swiper/css/scrollbar';
 
 const Slider = () => {
     return(
