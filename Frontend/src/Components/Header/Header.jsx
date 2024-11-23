7579174071fe0db7f10fd4ededbc3c6dadf07299
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-11-23 23:54:01 +0900

    Update Header.jsx

--- Frontend/src/Components/Header/Header.jsx
+++ Frontend/src/Components/Header/Header.jsx
@@ -1,34 +1,16 @@
-import './Header.css';
 const Header = () => {
     return(
-        <div>
-            <div className='Header--Container'>
-                <div className='Header--Container_Icon'></div>
-                <a className='Header--Container_Link_Orange ' href="#!">
-                    <p className='Header--Container_Link_Orange_Text'>Главная</p>
-                </a>
-                <a className='Header--Container_Link' href="#!">
-                    <p className='Header--Container_Link-Text'>Проекты</p>
-                </a>
-                <a className='Header--Container_Link' href="#!">
-                    <p className='Header--Container_Link-Text'>Жалобы</p>
-                </a>
-                <a className='Header--Container_Link' href="#!">
-                    <p className='Header--Container_Link-Text'>Новости</p>
-                </a>
-                <a className='Header--Container_Link' href="#!">
-                    <p className='Header--Container_Link-Text'>Контакты</p>
-                </a>
-                <a className='Header--Container_Link' href="#!">
-                    <p className='Header--Container_Link-Text'>Форум</p>
-                </a>
-                <a className='Header--Container_Link' href="#!">
-                    <p className='Header--Container_Link-Text'>Услуги</p>
-                </a>
-                <a className='Header--Container_Link-Cabinet' href="">
-                    <p className='Header--Container_Link-Text'>Личный кабинет</p>
-                </a>
-            </div>
+        <div className="Header--Container">
+            <span>
+                <button><img alt="Logo"/></button>
+                <button>Главная</button>
+                <button>Проекты</button>
+                <button>Жвлобы</button>
+                <button>Контакты</button>
+                <button>Форум</button>
+                <button>Услуги</button>
+            </span>
+            <span>Личный Кабинет</span>
         </div>
     );
 }
