0c977d0aa2395cbdcf3017f73445cad8060aaecc
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-11-24 01:52:45 +0900

    Create Goals.jsx

---
+++ Frontend/src/Components/Goals/Goals.jsx
@@ -0,0 +1,16 @@
+import './Goals.css';
+
+const Goals = () => {
+    return(
+        <div className="Goals--Container">
+            <div>
+                <h1>Наши основные цели</h1>
+                <p>Наша миссия – устанавливать самые высокие стандарты в строительной сфере.</p>
+            </div>
+
+            <div>
+
+            </div>
+        </div>
+    );
+}
