6ca924faf3f2ce0345bd03b8053229f6fa2f039a
Just Alex <just.alexdev@gmail.com>
2024-11-24 12:52:36 +0900

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,33 @@
-import { ip, serverPort } from './config/config';
-
-const express = require('express');
-const sequelize = require('./config/db');
-const authRoutes = require('./routes/authRoutes');
-const adminRoutes = require('./routes/adminRoutes');
-const newsRoutes = require('./routes/newsRoutes');
-const feedbackRoutes = require('./routes/feedbackRoutes');
-const swagger = require('./swagger');
+import { ip, serverPort } from './config.js';
+import express from 'express';
+import sequelize from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+import adminRoutes from './routes/adminRoutes.js';
+import newsRoutes from './routes/newsRoutes.js';
+import feedbackRoutes from './routes/feedbackRoutes.js';
+import swagger from './swagger.js';
+import basicAuth from 'express-basic-auth';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 const app = express();
 const PORT = serverPort;
 
+// Получаем текущий путь к файлу
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // Добавьте это для обработки данных формы
+app.use(express.static(path.join(__dirname, 'public'))); // Serve static files from the 'public' directory
 app.use('/api', authRoutes);
-app.use('/admin', adminRoutes);
+app.use('/admin', basicAuth({
+  users: { 'admin': 'admin' },
+  challenge: true,
+  unauthorizedResponse: (req) => {
+    return 'Unauthorized';
+  }
+}), adminRoutes);
 app.use('/api', newsRoutes);
 app.use('/api', feedbackRoutes);
 
@@ -23,8 +37,12 @@ app.get('/', (req, res) => {
   res.redirect('/api-docs');
 });
 
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
 sequelize.sync().then(() => {
   app.listen(PORT, () => {
-    console.log(`СЕРВЕР СЛУШАЕТСЯ ПО АДРЕСУ ${ip}${PORT}`);
+    console.log(`СЕРВЕР СЛУШАЕТСЯ ПО АДРЕСУ ${ip}:${PORT}`);
   });
 });
