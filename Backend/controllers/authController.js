e078e70f8fae696fca3b33e9f7e0d9de7aaad95c
Just Alex <just.alexdev@gmail.com>
2024-11-24 12:51:22 +0900

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,15 +1,22 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+// authController.js
+import jwt from 'jsonwebtoken';
+import User from '../models/User.js';
 
-const login = async (req, res) => {
+export const login = async (req, res) => {
+  console.log('Request body:', req.body); // Добавьте это для отладки
+
   const { username, password } = req.body;
-  const user = await User.findOne({ where: { username, password } });
-  if (user) {
+
+  // Проверка на наличие поля username
+  if (!username) {
+    return res.status(400).json({ message: 'Username is required' });
+  }
+
+  const user = await User.findOne({ where: { username } });
+  if (user && user.password === password) {
     const token = jwt.sign({ userId: user.id }, 'secret_key');
     res.json({ token });
   } else {
     res.status(401).json({ message: 'Invalid credentials' });
   }
 };
-
-module.exports = { login };
