0d953d121e459cea6b105c80ce4968e71263f087
Just Alex <just.alexdev@gmail.com>
2024-11-24 12:51:55 +0900

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,5 +1,6 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+// User.js
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/db.js';
 
 const User = sequelize.define('User', {
   username: {
@@ -13,4 +14,4 @@ const User = sequelize.define('User', {
   }
 });
 
-module.exports = User;
+export default User;
